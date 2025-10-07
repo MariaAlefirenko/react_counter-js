52446e263dee6fe986217d8e2ff613d8e7f5ed6e
Mariia Alefirenko <mari.alefir@gmail.com>
2025-10-07 18:16:52 +0300

    improved solution

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,11 @@ export const App = () => {
   const add100 = () => setCount(prev => prev + 100);
 
   const increase = () => {
-    if (count % 5 === 0) add100();
-    addOne();
+    setCount(prev => {
+      const next = prev + 1;
+
+      return next % 5 === 0 ? next + 100 : next;
+    });
   };
 
   return (
