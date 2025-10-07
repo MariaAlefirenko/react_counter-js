6d1672594a4f9c51e1e7ac0a722d25d3afcb58ef
Mariia Alefirenko <mari.alefir@gmail.com>
2025-10-07 18:24:11 +0300

    more improved solution

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,8 @@ export const App = () => {
   const addOne = () => setCount(prev => prev + 1);
   const add100 = () => setCount(prev => prev + 100);
 
-  const increase = () => {
-    setCount(prev => {
-      const next = prev + 1;
-
-      return next % 5 === 0 ? next + 100 : next;
-    });
-  };
+  const increase = () =>
+    setCount(prev => prev + (prev % 5 === 0 ? 100 : 0) + 1);
 
   return (
     <div className="App">
